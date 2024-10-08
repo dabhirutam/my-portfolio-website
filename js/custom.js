382de47ac7cfd84ad6a710145c7ab0b02d7b31e1
dabhirutam <dabhirutam34@gmail.com>
2024-10-08 18:59:55 +0530

    Add-Certificates

--- js/custom.js
+++ js/custom.js
@@ -1,71 +1,24 @@
-// loder-js
-$(window).on('load', function () {
-    $('#lodder-wapper').delay(5000).fadeOut(1000);
-})
-// scroll-header-js
-$(window).on('scroll', function () {
-    if ($(window).scrollTop()) {
-        $('header').addClass('scroll-header');
-    }
-    else {
-        $('header').removeClass('scroll-header');
-    }
+let skill = document.getElementById('skill');
+let skillIMG = ['html 5', 'css 3', 'jquery', 'bootstrap', 'c-language', 'github', 'javascript', 'python', 'dsa with c++'];
+
+// Skills js
+skillIMG.forEach((logo) => {
+    skill.innerHTML += `<div class="item my-3">
+                            <div class="bg-primary bg-opacity-50 shadow py-4 rounded">
+                                <img class="w-90 mx-auto rounded-3" src="images/skills/${logo}.png" alt="">
+                                <h4 class="mt-3 text-uppercase">${logo}</h4>
+                            </div>
+                       </div>`;
 });
-// scroll-btn-js
-$(window).scroll(function () {
-    if ($(window).scrollTop() > 200) {
-        $('#scroll-btn').fadeIn(200)
-    }else {
-        $('#scroll-btn').fadeOut();
-    };
-});
-$(document).ready(function () {
-    // md-menus-js
-    $('#md-menus-open').click(function () {
-        $('#md-menus').toggle(500);
-        $(this).children('i').toggle(1);
-    });
-    // owl-carosel-js
-    var owl = $('.owl-carousel');
-    owl.owlCarousel({
-        items: 3,
-        loop: true,
-        autoplay: true,
-        margin: 20,
-        autoplayTimeout: 2000,
-        autoplayHoverPause: true,
-        responsive: {
-            900: {
-                items: 3,
-            },
-            450: {
-                items: 2,
-            },
-            0: {
-                items: 1,
-            },
-        },
-    });
-    // section - hover - js
-    var section = $('section')
-        , nav = $('nav')
-        , nav_height = nav.outerHeight();
 
-    $(window).on('scroll', function () {
-        var cur_pos = $(this).scrollTop();
+// Certificates modal js
+$('.cert').click(function () {
+    let certIMG = document.getElementById('certIMG');
+    let certTitle = document.getElementById('certTitle');
 
-        section.each(function () {
-            var top = $(this).offset().top - nav_height,
-                bottom = top + $(this).outerHeight();
+    let img = $(this).siblings('img');
+    let title = $(this).siblings('h3').children('.title');
 
-            if (cur_pos >= top && cur_pos <= bottom) {
-                nav.find('a').removeClass('active');
-                nav.find('a').children('span').removeClass('active');
-
-                $(this).addClass('');
-                nav.find('a[href="#' + $(this).attr('id') + '"]').addClass('active');
-                nav.find('a[href="#' + $(this).attr('id') + '"]').children('span').addClass('active');
-            };
-        });
-    });
+    certTitle.innerHTML = "Certificate of " + title[0].firstChild.data;
+    certIMG.innerHTML = `<img class="border-primary border shadow-lg rounded-3" src="${img[0].attributes.src.nodeValue}" width="100%">`;
 });
